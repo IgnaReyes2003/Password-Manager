34ebcc7b98162bcd80776387a16cc459ee1531ea
IgnacioRisso <gamerdream406@gmail.com>
2023-02-11 23:25:47 -0300

    Updates

--- Usuario.py
+++ Usuario.py
@@ -17,5 +17,26 @@ def registrar(nombre, apellido, contrasena_maestra):
         sentencia_sql = '''INSERT INTO usuario
         (nombre, apellido, contrasena_maestra) 
         VALUES (?, ?, ?)'''
-        cm_cifrada = hashlib.sha256(contrasena_maestra.encode("utf_8")).hexadigest()
-        datos = nombre
+        cm_cifrada = hashlib.sha256(contrasena_maestra.encode("utf-8")).hexdigest()
+        datos = (nombre, apellido, cm_cifrada)
+        cursor.execute(sentencia_sql, datos)
+        conexion.commit()
+        conexion.close()
+        return "Registro correcto"
+
+    except Error as err:
+        return "Ha ocurrido un error "+str(err)
+
+def comprobar_contrasena(id, contrasena_maestra):
+    try:
+        conexion = conectar()
+        cursor = conexion.cursor()
+        sentencia_sql = '''SELECT * FROM usuario WHERE id=? AND contrasena_maestra=?'''
+        cm_cifrada = hashlib.sha256(contrasena_maestra.encode("utf-8")).hexdigest()
+        cursor.execute(sentencia_sql, (id, cm_cifrada))
+        datos = cursor.fetchall()
+        conexion.close()
+        return datos
+    except Error as err:
+        return "Ha ocurrido un error "+ str(err)
+
